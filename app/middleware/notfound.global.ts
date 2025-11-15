2e19a02e810bc48fc60a54a74625fd91ad237ef9
orderlyproblemsolvers <orderlyproblemsolvers@gmail.com>
2025-11-15 09:25:03 +0100

    finals

--- app/middleware/notfound.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const router = useRouter()
-  const routeExists = router.getRoutes().some(r => r.path === to.path)
-
-  if (!routeExists) {
-    return navigateTo('/', { replace: true })
-  }
-})
